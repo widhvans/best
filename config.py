54fa527ead449d56df4ed90c995bed0a7f565716
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-18 21:22:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,4 +33,4 @@ class Config:
     # VVVVVV YAHAN PAR NAYA TUTORIAL LINK ADD KIYA GAYA HAI VVVVVV #
     # ================================================================= #
     # Yahan apna tutorial video ya channel ka link daalein
-    TUTORIAL_URL = os.environ.get("TUTORIAL_URL", "https://t.me/your_tutorial_link_here")
+    TUTORIAL_URL = os.environ.get("TUTORIAL_URL", "https://t.me/tutorial_really/2")
